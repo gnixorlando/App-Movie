fc3fb236f40d4591bdf2ab6f98b8967e925f8099
gabyorlando <gabyorlandonascimento1@gmail.com>
2025-03-28 15:44:09 -0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+export function Header() {
+    return (
+        <Fragment>
+            <h1> Aplicação de filmes</h1 >
+            <ul>
+                <li><Link to={"/"}>Home</Link></li>
+                <li><Link to={"/movies"}>Movies</Link></li>
+            </ul>
+        </Fragment>
+    )
+}
